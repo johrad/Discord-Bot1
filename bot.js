9098c30237fbb3b6ead6b9cafbfa250a293e7fb4
johrad <jpdragic@gmail.com>
2018-11-16 11:42:14 +0100

    test1

---
+++ bot.js
@@ -0,0 +1,28 @@
+// Require token file
+require('./TOKEN.js');
+
+// Import the discord.js module
+'use strict';
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+
+// Logging in to discord
+client.on('ready', () => {
+  console.log('');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
+});
+
+client.login(TOKEN);
+
+client.off;
---
+++ test.js
@@ -0,0 +1,3 @@
+require("./TOKEN.js");
+
+console.log(TOKEN);
