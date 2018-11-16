cd924c428fc35ccdd538505f80cd6db2a9b05297
johrad <jpdragic@gmail.com>
2018-11-16 13:29:55 +0100

    update

--- bot.js
+++ bot.js
@@ -1,27 +1,34 @@
-// Require token file
-require('./TOKEN.js');
+// Require config file
+const config = require('./config.json');
 
 // Import the discord.js module
-'use strict';
 const Discord = require('discord.js');
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
-
 
 // Logging in to discord
 client.on('ready', () => {
   console.log('');
 });
 
+
+// ################ THE REAL MEAT ################
+
 // Create an event listener for messages
 client.on('message', message => {
   // If the message is "ping"
-  if (message.content === 'ping') {
+  if (["fortnite", "cancer", "floss"].includes(message.content)) {
     // Send "pong" to the same channel
-    message.channel.send('pong');
+    message.channel.send('https://gfycat.com/MeagerBrokenJackrabbit');
   }
+
+  if (message.content.startsWith(config.prefix + "help")){ //prefix is imported from config.json
+    message.channel.send("you fool, I dont help people like you");
+  }
+  
 });
 
-client.login(TOKEN);
+client.login(config.token);
+
 
