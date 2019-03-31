06483dfbb31ef7ac0f48ad76e7010566e7c8cfa7
johrad <jpdragic@gmail.com>
2019-03-31 22:10:10 +0200

    upgraded the boy

--- bot.js
+++ bot.js
@@ -9,6 +9,9 @@ const Discord = require('discord.js');
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
+
+
+
 // Logging in to discord
 client.on('ready', () => {
   console.log('');
@@ -28,9 +31,33 @@ client.on('message', message => {
 
   // ADD "IF USER = !BOT, DO OO SHIT"
 
+  if (message.author.bot) return; // cheks if user if bot, if bot, stops and goes back
+
   if (["fortnite", "cancer", "floss"].includes(message.content)) {
     // Send "pong" to the same channel
     message.channel.send('https://gfycat.com/MeagerBrokenJackrabbit');
+  }
+
+  if (["simon", "Simon"].includes(message.content)) {
+    // Send "pong" to the same channel
+    message.channel.send({
+      files: ['./benis.mp3']
+    });
+  }
+
+  if (["selfie", "Selfie"].includes(message.content)) {
+    // Send "pong" to the same channel
+    message.channel.send({
+      files: ['./img.png']
+    });
+    console.log(message.author.id, "told me to send a selfie UwU");
+  }
+
+  if (message.isMentioned(client.users.get('511473649803329565'))) {
+    console.log("somebody pinged me!, the user was", message.author.id);
+    console.log("Their nickname was", message.author.nicknam, "And they said:");
+    console.log('\x1b[31m%s\x1b[0m', message.content, '\x1b[33m"\nSaved to the log (that doesnt exist yet lol)\n"\x1b[0m'); // "\x1b[31m" sets color 
+
   }
 
   if (message.content.startsWith(config.prefix + "help")) { //prefix is imported from config.json
@@ -42,4 +69,5 @@ client.on('message', message => {
   }
 });
 
+console.log("No errors yet, procceding...");
 client.login(config.token);
